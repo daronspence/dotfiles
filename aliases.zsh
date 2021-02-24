0dba3122989e6c5ee0e116a536e837e2a00c07ba
Daron Spence <daronspence@gmail.com>
2021-02-24 12:55:41 -0600

    updates

--- aliases.zsh
+++ aliases.zsh
@@ -1,9 +1,9 @@
 # Shortcuts
 alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
-alias reloadcli="source $HOME/.zshrc"
+alias reloadshell="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -ahlF --color --group-directories-first"
-weather() { curl -4 wttr.in/${1:-antwerp} }
+weather() { curl -4 wttr.in/${1:-dallas} }
 alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
